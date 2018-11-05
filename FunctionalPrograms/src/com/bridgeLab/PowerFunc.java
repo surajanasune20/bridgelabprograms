fe8277d36da78d38f03c53f598c6037327ef791c
suraj <suraj@gmail.com>
2018-11-05 10:15:20 +0530

    First commit...

---
+++ FunctionalPrograms/src/com/bridgeLab/PowerFunc.java
@@ -0,0 +1,19 @@
+package com.bridgeLab;
+
+public class PowerFunc {
+	int power(int n){
+		{
+			if(n >= 0 && n<=31)
+			{
+				n=power(0)*2;
+			}
+			else
+			{
+				
+			}
+		}
+		return n;
+	}
+	
+	
+}
